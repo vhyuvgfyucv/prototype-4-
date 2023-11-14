72fab334995705a8323cd4ee10be7e786a350916
vhyuvgfyucv <brightonmackenzie496@riversideunified.org>
2023-11-14 14:24:42 -0800

    4.2 starting step 4

--- prototype 4/Assets/Scripts/Enemy.cs
+++ prototype 4/Assets/Scripts/Enemy.cs
@@ -6,15 +6,18 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 3.0f;
     private Rigidbody enemyRb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed); 
     }
 }
