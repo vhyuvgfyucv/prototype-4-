efe5865cee52f4d3384932e38609497162ab7b20
vhyuvgfyucv <brightonmackenzie496@riversideunified.org>
2023-11-14 13:49:15 -0800

    starting 4.2

---
+++ prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
