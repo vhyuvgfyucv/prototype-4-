63fa3c226a4e4af913b3395f134d7be7de6e58bc
vhyuvgfyucv <brightonmackenzie496@riversideunified.org>
2023-11-17 14:13:35 -0800

    4.3 step 3 starting

--- prototype 4/Assets/Scripts/PlayerControl.cs
+++ prototype 4/Assets/Scripts/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-
+    public bool hasPowerup = false;
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
@@ -21,5 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+
     }
 }
