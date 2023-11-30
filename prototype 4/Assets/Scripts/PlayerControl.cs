4147b660ab71e336d4eedf0c06d5fc07eb824e74
RzrRacer88 <146136167+RazrRacer88@users.noreply.github.com>
2023-11-30 14:25:38 -0800

    step 5

--- prototype 4/Assets/Scripts/PlayerControl.cs
+++ prototype 4/Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public bool hasPowerup = false;
+    private float powerUpStrength = 450.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
@@ -18,17 +19,38 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
+    }
 
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyrigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyrigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("collided with: " + collision.gameObject.name + "with powerup set to" + hasPowerup);
+        }
     }
 }
